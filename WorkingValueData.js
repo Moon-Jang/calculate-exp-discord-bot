342bc060a7c9216e8c20159cfdc921d291fdd93c
MoonJang <roby8502@naver.com>
2022-06-21 22:17:48 +0900

    260 ~ 265 value 업데이트

--- WorkingValueData.js
+++ WorkingValueData.js
@@ -4,7 +4,12 @@ const exp = 51428464810
 
 WorkingValueData[258] = 4.042
 WorkingValueData[259] = 4.042
-WorkingValueData[260] = 2.0572
+WorkingValueData[260] = 5.5
+WorkingValueData[261] = 5.5
+WorkingValueData[262] = 5.5
+WorkingValueData[263] = 5.5
+WorkingValueData[264] = 5.5
+WorkingValueData[265] = 5.5
 
 WorkingValueData.moneyPerHour = 3000
 
