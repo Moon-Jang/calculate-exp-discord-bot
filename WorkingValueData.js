128666ccc7a6bd1885e26e69b10b89b56c277adb
MoonJang <roby8502@naver.com>
2022-06-23 22:26:29 +0900

    경험치 비율 조정

--- WorkingValueData.js
+++ WorkingValueData.js
@@ -4,12 +4,12 @@ const exp = 51428464810
 
 WorkingValueData[258] = 4.042
 WorkingValueData[259] = 4.042
-WorkingValueData[260] = 5.5
-WorkingValueData[261] = 5.5
-WorkingValueData[262] = 5.5
-WorkingValueData[263] = 5.5
-WorkingValueData[264] = 5.5
-WorkingValueData[265] = 5.5
+WorkingValueData[260] = 5.0
+WorkingValueData[261] = 5.0
+WorkingValueData[262] = 5.0
+WorkingValueData[263] = 5.0
+WorkingValueData[264] = 5.0
+WorkingValueData[265] = 5.0
 
 WorkingValueData.moneyPerHour = 3000
 
